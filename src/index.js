82f75b61c017f27114c5709e81b9a2ef8e1fdd1d
cooper <cooper.elliott30@gmail.com>
2023-02-28 14:20:12 +1100

    Add filter by date with date-fns

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import { render } from './render.js';
 import { createNewItem } from './todoFactory.js';
+import { format, parseISO } from 'date-fns';
 
 // todo items
 const todoItems = [];
 
+// add placeholder notes
 createNewItem(todoItems, 'Go Shopping', "I'm hungry.", '2023-09-07', 'urgent');
 createNewItem(todoItems, 'Soccer Training', 'Kick ball.', '2023-02-28');
+createNewItem(todoItems, 'Gym', 'Legs', '2023-03-01');
+console.log(todoItems);
 
-// todoItems.push(
-//   createNewItem('Go Shopping', "I'm hungry.", '01/01/01', 'urgent')
-// );
-// todoItems.push(createNewItem('Soccer Training', 'Kick ball.', '02/01/01'));
+// render home notes i.e. all notes
+render(todoItems);
+
+// filter buttons
+const homeButton = document.querySelector('#home');
+const todayButton = document.querySelector('#today');
+const weekButton = document.querySelector('#week');
+
+homeButton.addEventListener('click', (event) => render(todoItems));
+todayButton.addEventListener('click', filterByToday);
+weekButton.addEventListener('click', filterByWeek);
+
+function filterByToday(event) {
+  let dateToday = format(new Date(), 'yyyy-MM-dd');
+  console.log(dateToday);
+  render(todoItems.filter((item) => item.dueDate === dateToday));
+}
+
+function filterByWeek(event) {
+  let weekToday = format(new Date(), 'ww');
+  console.log(weekToday);
+  render(
+    todoItems.filter(
+      (item) => format(parseISO(item.dueDate), 'ww') === weekToday
+    )
+  );
+}
 
 // get todo info from form
 const addNoteForm = document.querySelector('#note-form');
@@ -42,7 +69,3 @@ function getTodoInfo(event) {
 
   render(todoItems);
 }
-
-console.log(todoItems);
-
-render(todoItems);
--- src/render.js
+++ src/render.js
@@ -1,9 +1,9 @@
 // render todo list on page
-function render(todoItems) {
+function render(array) {
   let notesSection = document.querySelector('.notes');
   notesSection.replaceChildren();
 
-  for (let i = 0; i < todoItems.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     let div = document.createElement('div');
     div.classList.add('todo-item');
     let container = document.createElement('div');
@@ -11,9 +11,9 @@ function render(todoItems) {
     let due = document.createElement('p');
     let des = document.createElement('p');
 
-    h2.innerText = todoItems[i].title;
-    due.innerText = todoItems[i].dueDate;
-    des.innerText = todoItems[i].description;
+    h2.innerText = array[i].title;
+    due.innerText = array[i].dueDate;
+    des.innerText = array[i].description;
 
     container.append(h2, des);
     div.append(container, due);
